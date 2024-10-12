a7ac4cf13606d17c0c95bd0cd8ff6705c79c628b
Noor8151 <premb4999@gmail.com>
2024-10-12 22:05:18 +0530

    Update Booking.java

--- entity/Booking.java
+++ entity/Booking.java
@@ -1,63 +1,21 @@
 package entity;
 
-import entity.Flight;
-import entity.Customer;
+class Booking {
+    private Passenger passenger; // Has-a relationship
+    private Flight flight; // Has-a relationship
 
-public class Booking {
-	int numTickets;
-	int addFlight;
-	String updateFlight;
-	String cancleFlight;
+    public Booking(Passenger passenger, Flight flight) {
+        this.passenger = passenger;
+        this.flight = flight;
+    }
 
-	public Booking(int numTickets, int addFlight, String updateFlight, String cancleFlight) {
-		super();
-		this.numTickets = numTickets;
-		this.addFlight = addFlight;
-		this.updateFlight = updateFlight;
-		this.cancleFlight = cancleFlight;
-		int flightId;
-		int customerId;
-	}
-
-	public int getNumTickets() {
-		return numTickets;
-	}
-
-	public void setNumTickets(int numTickets) {
-		this.numTickets = numTickets;
-	}
-
-	public int getAddFlight() {
-		return addFlight;
-	}
-
-	public void setAddFlight(int addFlight) {
-		this.addFlight = addFlight;
-	}
-
-	public String getUpdateFlight() {
-		return updateFlight;
-	}
-
-	public void setUpdateFlight(String updateFlight) {
-		this.updateFlight = updateFlight;
-	}
-
-	public String getCancleFlight() {
-		return cancleFlight;
-	}
-
-	public void setCancleFlight(String cancleFlight) {
-		this.cancleFlight = cancleFlight;
-	}
-
-	@Override
-	public String toString() {
-		return "Booking [numTickets=" + numTickets + ", addFlight=" + addFlight + ", updateFlight=" + updateFlight
-				+ ", cancleFlight=" + cancleFlight + ", getNumTickets()=" + getNumTickets() + ", getAddFlight()="
-				+ getAddFlight() + ", getUpdateFlight()=" + getUpdateFlight() + ", getCancleFlight()="
-				+ getCancleFlight() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
-				+ super.toString() + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "passenger=" + passenger +
+                ", flight=" + flight.getFlightNumber() +
+                '}';
+    }
 }
+
+
